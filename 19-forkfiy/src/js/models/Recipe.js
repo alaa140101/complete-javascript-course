0f1a6d829bfe9319873d4b3e477c1199e7e1b064
alaa140101 <alaa140011@gmail.com>
2020-06-21 15:25:40 +0300

    start lec-148

--- 19-forkfiy/src/js/models/Recipe.js
+++ 19-forkfiy/src/js/models/Recipe.js
@@ -1,6 +1,32 @@
-/*
+import axios from 'axios';
 
-const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
 
+  async getRecipe(){
 
-*/
+    try {
+      const result = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+      this.title = result.data.recipe.title;
+      this.author = result.data.recipe.publisher;
+      this.img = result.data.recipe.image_url;
+      this.url = result.data.recipe.source_url;
+      this.ingredients = result.data.recipe.ingredients;
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  calcTime(){
+    // Assuming that we need 15 min for 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings(){
+    this.servings = 4;
+  }
+}
