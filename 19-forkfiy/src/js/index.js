684f7bb1918c755c551b7f15758e129d9f167954
alaa140101 <alaa140011@gmail.com>
2020-06-21 13:28:23 +0300

    Start lec147

---
+++ 19-forkfiy/src/js/index.js
@@ -0,0 +1,49 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) New search object and state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+})
+
+elements.searchResPages.addEventListener('click', e => {
+  e.preventDefault();
+  const btn = e.target.closest('.btn-inline');
+  if(btn){
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage)
+    console.log(goToPage);
+  }
+})
