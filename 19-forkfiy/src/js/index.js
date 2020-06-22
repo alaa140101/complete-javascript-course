840b72de9d775c5123b9d5666fa5f437a71b6951
alaa140101 <alaa140011@gmail.com>
2020-06-22 06:49:59 +0300

    End lec-148

--- 19-forkfiy/src/js/index.js
+++ 19-forkfiy/src/js/index.js
@@ -27,12 +27,17 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4) Search for recipes
-    await state.search.getResults();
-
-    // 5) Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
+  
+      // 5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (error) {
+      alert('Somthing went wrong with search.');
+      clearLoader();
+    }
   }
 }
 
@@ -55,6 +60,31 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * RECIPE CONTROLLER
  */
-const r = new Recipe('8f3e73');
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+    // Prepare Ui for changes
+
+    // Create new recipe object 
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+  
+      // Calcauate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+  
+      // Render recipe
+      console.log(state.recipe);
+    } catch (error) {
+      alert('Error catching recipe.');
+    }
+  }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
